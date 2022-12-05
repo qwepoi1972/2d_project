336b3c6e6d71ace9bd54ca9363ae305668711774
NewWorldBlues <trubeev7@gmail.com>
2022-12-05 21:15:25 +0300

    Добавлена случайная генерация платформ с промежутком в 100 пикселей

--- new_main.py
+++ new_main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 from math import copysign
 FPS = 60
 size = [800, 600]
@@ -16,8 +17,8 @@ class Player(pygame.sprite.Sprite):
         self.reflection = False
         self.speed_x = 0
         self.speed_y = 0
-        self.speed_max = 10
-        self.speed_jump = 10
+        self.speed_max = 15
+        self.speed_jump = 15
 
     def draw(self, screen):
         screen.blit(pygame.transform.flip(self.image, self.reflection, False), self.rect)
@@ -66,23 +67,33 @@ def move(player, platforms):
     player.rect[0] += round(player.speed_x)
 
 
+def spawn_start():
+    platforms = []
+    for i in range(7):
+        platforms.append(Platform(screen=screen, pos_x=randint(50, 750),
+                                  pos_y=-50 + 100 * i, width=120, height=20))
+    return platforms
+
+
+def changing_platforms(platforms):
+    for i in range(len(platforms)):
+        if platforms[i].rect[1] > 600:
+            platforms[i] = Platform(screen=screen, pos_x=randint(50, 750),
+                                    pos_y=-50, width=120, height=20)
+
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     finished = False
-    player = Player(screen, 700, 500)
-    platform1 = Platform(screen, 650, 550, 120, 20)
-    platform2 = Platform(screen, 600, 500, 120, 20)
-    platform3 = Platform(screen, 550, 450, 120, 20)
-    platform4 = Platform(screen, 500, 400, 120, 20)
-    platform5 = Platform(screen, 450, 350, 120, 20)
-    platform6 = Platform(screen, 400, 300, 120, 20)
-    platform7 = Platform(screen, 350, 250, 120, 20)
-    platforms = [platform1, platform2, platform3, platform4, platform5, platform6, platform7]
+    platforms = spawn_start()
+    player = Player(screen=screen, start_x=platforms[-1].rect[0],
+                    start_y=platforms[-1].rect[1] - 50)
     while not finished:
         clock.tick(FPS)
         pygame.display.update()
         screen.fill(white)
+        changing_platforms(platforms)
         move(player, platforms)
         player.draw(screen)
         for plat in platforms:
